3b2f2fcd14fe03189cd85c8215e1b7feffbdc840
Matilde Aragao <matilde.aragao@grupofullsix.pt>
2020-04-22 14:20:04 +0100

    heroku init

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan( ":method :url :status :res[content-length] - :response-time ms :content"))
 
 morgan.token("content", req => {
@@ -95,7 +97,7 @@ app.post('/api/persons', (request, response) => {
   })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
