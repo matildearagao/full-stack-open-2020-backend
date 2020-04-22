6a9af07115fc7e3ff18e1f74ad08201bcdce7074
Matilde Aragao <matilde.aragao@grupofullsix.pt>
2020-04-22 15:42:15 +0100

    changes in build

---
+++ build/precache-manifest.559c41c6821bd2a5be9f1e1099211a44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03604944352a5f693051892f379989d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f159e2268ca818c45d4",
+    "url": "/static/css/main.444909ce.chunk.css"
+  },
+  {
+    "revision": "50b2e459d25667e2841c",
+    "url": "/static/js/2.37c0a557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37c0a557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f159e2268ca818c45d4",
+    "url": "/static/js/main.ff02d1a7.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
